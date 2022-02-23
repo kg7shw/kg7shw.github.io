45b157438cf289b9b65b9ada5cbfca8d50b6aea0
kg7shw <kg7shw.gmail.com>
2022-02-22 21:10:44 -0700

    push

---
+++ python/CSE 110/id_card.py
@@ -0,0 +1,22 @@
+print('Please enter the following information: ')
+print()
+
+first_name = input('First name: ')
+last_name = input('Last name: ')
+email_address = input('Email address: ')
+phone_number = input('Phone Number: ')
+job_title = input('Job title: ')
+id_number = input('ID Number: ')
+
+print('The ID Card is: ')
+print('----------------------------------------')
+print(f'{last_name.upper()}, {first_name.capitalize()}')
+
+
+print(job_title)
+print(f'ID: {id_number} ')
+print()
+
+print(email_address.lower())
+print(phone_number)
+print('----------------------------------------')
---
+++ python/favorite_color.py
@@ -0,0 +1,8 @@
+# input for users Name
+name = input('Enter your name: ')
+
+#User input for favorite color
+favorite_color = input('Enter your favorite color: ')
+
+# Total Output
+print(name + ' your Favorite color is: ' + favorite_color)
