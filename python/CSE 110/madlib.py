1387c1268664d243bd01f778326700ac34d97060
kg7shw <kg7shw.gmail.com>
2022-02-26 18:45:31 -0700

    push

---
+++ python/CSE 110/madlib.py
@@ -0,0 +1,9 @@
+print('Enter the following: ')
+
+adjective = input('Adjective: ')
+noun = input('Noun: ')
+verb = input('Verb: ')
+verb_past = input('Verb, past tense: ')
+print('Your madlib story is: ')
+
+print(f"Yesterday my brother and I went to the park. We saw a {adjective} {noun} running up and down the greenway. The  {adjective} {noun}  {verb}, through the playground that ran through the middle of the park. We got out some snacks and tried to feed a {noun}. After feeding the animals we felt hungry so we went to get some {adjective} burgers and fries. After finishing eating we had to {verb} to get home on time so we didn't miss our favorite tv show. When we got home we {verb_past} our mom and sat down to watch our show.")
