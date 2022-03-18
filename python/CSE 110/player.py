1fa765427e9ff33a5339be60d388b288f171bf74
kg7shw <kg7shw.gmail.com>
2022-03-18 15:59:06 -0600

    new changes

---
+++ python/CSE 110/player.py
@@ -0,0 +1,24 @@
+# from lib2to3.pytree import LeafPattern
+# import math
+# import random
+
+# class player:
+#     def _init_(self, letter):
+#         #letter is x or o
+#         self.letter = Letter 
+
+# # all players can move
+# def get_move(self, game):
+#     pass
+# class RandomComputerPlayer(player):
+#     def _init_(self, letter):
+#         super()._init_(letter)
+
+#     def get_move(self, game):
+#         pass
+# class HumanPlayer(player):
+#     def _init_(self, letter):
+#         super()._init_(letter)
+
+#     def get_move(self, game):
+#         pass
